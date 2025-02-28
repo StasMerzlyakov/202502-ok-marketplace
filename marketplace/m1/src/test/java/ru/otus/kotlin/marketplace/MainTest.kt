9a4c7ed3332e73fe54556a1f5e4c69d4a0794a17
Stanislav Merzlyakov <st.merzlyakov@yandex.ru>
2025-02-28 14:00:57 +0400

    m1

---
+++ marketplace/m1/src/test/java/ru/otus/kotlin/marketplace/MainTest.kt
@@ -0,0 +1,11 @@
+package ru.otus.kotlin.marketplace
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+    @Test
+    fun firstTest() {
+        assertEquals(8, 4 * 2)
+    }
+}
