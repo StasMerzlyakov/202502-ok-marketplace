9cc613af05ff5aa0f959b00f0140d63a07000af4
Stanislav Merzlyakov <st.merzlyakov@yandex.ru>
2025-05-07 13:30:27 +0400

    нагуглил как выгрузить данные пользователей в keycloak теперь можно спокойно перезапускать контейнеры

--- meethere/deploy/README.md
+++ meethere/deploy/README.md
@@ -25,7 +25,15 @@ docker-compose down
 ```
 
 # добавление realm meethere, и users - после изменений не забыть выгрузить настройки
-http://localhost:8080/admin  (admin:admin; docker-compose.yml)
+http://localhost:8080/admin/  (admin:admin; docker-compose.yml)
 
+для экспорта realm вместе с пользователями
+```
+docker exec -it my_keycloak sh -c \
+  "cp -rp /opt/keycloak/data/h2 /tmp ; \
+  /opt/keycloak/bin/kc.sh export --dir /opt/keycloak/data/import --realm MY_REALM \
+    --db dev-file \
+    --db-url 'jdbc:h2:file:/tmp/h2/keycloakdb;NON_KEYWORDS=VALUE'"
+```
 
 
--- meethere/deploy/keycloak-tokens.sh
+++ meethere/deploy/keycloak-tokens.sh
@@ -3,22 +3,24 @@
 KCHOST=http://localhost:8080
 REALM=meethere
 CLIENT_ID=meethere-service
-UNAME=meethere-test
-PASSWORD=meethere
+UNAME=user
+PASSWORD=123456
 
-# shellcheck disable=SC2006
-#ACCESS_TOKEN=`curl \
-#  -d "client_id=$CLIENT_ID" -d "client_secret=$CLIENT_SECRET" \
-#  -d "username=$UNAME" -d "password=$PASSWORD" \
+
+#curl \
+#  -d "client_id=$CLIENT_ID" \
+#  -d "username=$UNAME" \
+#  -d "password=$PASSWORD" \
 #  -d "grant_type=password" \
-#  "$KCHOST/auth/realms/$REALM/protocol/openid-connect/token"  | jq -r '.access_token'`
+#  "$KCHOST/realms/$REALM/protocol/openid-connect/token"
+#exit
 
 ACCESS_TOKEN=`curl \
   -d "client_id=$CLIENT_ID" \
   -d "username=$UNAME" \
   -d "password=$PASSWORD" \
   -d "grant_type=password" \
-  "$KCHOST/realms/$REALM/protocol/openid-connect/token"  | jq -r '.access_token'`
+  "$KCHOST/realms/$REALM/protocol/openid-connect/token" | jq -r '.access_token'` 
 echo "$ACCESS_TOKEN"
 
 #curl \
